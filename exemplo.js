884c9f7f3946ae283a88bbc8a6cee8c1ae516711
LuizEdP <luiz.eduardo@mrpbrasil.com>
2014-09-26 12:05:13 -0300

    Ativar ciclo, pular, desativar ciclo, mover

---
+++ exemplo.js
@@ -0,0 +1,40 @@
+var resul = null;
+		
+$.ajax({	// Pegar ID's necessários para pular
+	type: 'GET',
+	url: 'https://plug.dj/_/rooms/state',
+	contentType: 'application/json',
+	data: '{"id": 1234567}'}
+	).done(function( msg ) {
+		resul = msg.data[0];
+		
+		$.ajax({	// Ativar o ciclo de DJ's
+			type: 'PUT',
+			url: 'https://plug.dj/_/booth/cycle',
+			contentType: 'application/json',
+			data: '{"shouldCycle":true}'}
+			).done(function( msg ) {
+				$.ajax({	// Pular o DJ
+					type: 'POST',
+					url: 'https://plug.dj/_/booth/skip',
+					contentType: 'application/json',
+					data: '{"userID":' + resul.booth.currentDJ + ',"historyID":"' + resul.playback.historyID + '"}'}
+					).done(function( msg ) {
+						$.ajax({	// Desativar o ciclo de DJ's
+							type: 'PUT',
+							url: 'https://plug.dj/_/booth/cycle',
+							contentType: 'application/json',
+							data: '{"shouldCycle":false}'}
+							).done(function( msg ) {
+								$.ajax({	// Mover o usuário (opcional)
+									type: 'POST',
+									url: 'https://plug.dj/_/booth/move',
+									contentType: 'application/json',
+									data: '{"userID":"' + resul.booth.currentDJ + '","position":0}'} // position 0 = move pra 1º na fila
+									).done(function( msg ) {
+										console.log(msg);
+								});
+						});
+				});
+		});
+});

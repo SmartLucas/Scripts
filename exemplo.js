4998fb023fd5e888d8df872522f8d25024bbea9e
LuizEdP <luiz.eduardo@mrpbrasil.com>
2014-09-26 14:13:32 -0300

    Update exemplo.js

--- exemplo.js
+++ exemplo.js
@@ -1,3 +1,7 @@
+Exemplo 1 - ativa o ciclo, pula o dj, desativa o ciclo, move para primeiro
+
+// início exemplo 1
+
 var resul = null;
 		
 $.ajax({	// Pegar ID's necessários para pular
@@ -37,3 +41,44 @@ $.ajax({	// Pegar ID's necessários para pular
 				});
 		});
 });
+
+
+// fim exemplo 1
+
+Exemplo 2 = ativa o ciclo, pula o dj, desativa o ciclo
+
+// início exemplo 2
+
+var resul = null;
+		
+$.ajax({	// Pegar ID's necessários para pular
+	type: 'GET',
+	url: 'https://plug.dj/_/rooms/state',
+	contentType: 'application/json',
+	data: '{"id": 1234567}'}
+	).done(function( msg ) {
+		resul = msg.data[0];
+		
+		$.ajax({	// Ativar o ciclo de DJ's
+			type: 'PUT',
+			url: 'https://plug.dj/_/booth/cycle',
+			contentType: 'application/json',
+			data: '{"shouldCycle":true}'}
+			).done(function( msg ) {
+				$.ajax({	// Pular o DJ
+					type: 'POST',
+					url: 'https://plug.dj/_/booth/skip',
+					contentType: 'application/json',
+					data: '{"userID":' + resul.booth.currentDJ + ',"historyID":"' + resul.playback.historyID + '"}'}
+					).done(function( msg ) {
+						$.ajax({	// Desativar o ciclo de DJ's
+							type: 'PUT',
+							url: 'https://plug.dj/_/booth/cycle',
+							contentType: 'application/json',
+							data: '{"shouldCycle":false}'								
+						});
+				});
+		});
+});
+
+// fim exemplo 2

cd9a0651c82b5b65790891ebb5aac4e69d794232
LuizEdP <luiz.eduardo@mrpbrasil.com>
2014-09-28 13:23:15 -0300

    Update mention.js

--- mention.js
+++ mention.js
@@ -8,4 +8,53 @@ javascript: (function(){
     }
 
     API.on(API.CHAT, function(_){ _.type === 'mention' && 
-                      document.getElementById('som' + (Math.floor(Math.random()*total)+1)).play(); }) })();
+                      document.getElementById('som' + (Math.floor(Math.random()*total)+1)).play(); });
+                      
+    function adicionar(val){
+        if ( isNaN(val))
+            return;
+        
+        if ( val <= total ){
+            API.chatLog('Nenhum som para adicionar!', true);
+            return;
+        }
+        
+        for (var i = total+1; i <= val; i++){
+             $('head').append(
+                  $('<audio/>', {
+                      id: 'som' + i, src: 'http://www.mrpbrasil.com/Caipira/sons/som' + i + '.mp3'})
+              );
+        }
+        total = val;
+        API.chatLog('Som(s) adicionado(s).', false);
+    };
+    
+    function tocar(val){
+        if ( val && val >= 1 && val <= total)
+            document.getElementById('som' + val).play();
+        else
+            API.chatLog('Valor inválido', true);
+    };
+    
+    function comando(obj){
+        var arr = obj.split(" ");
+        
+        if ( !arr )
+            return;
+        
+        if ( !arr[1]){
+            API.chatLog('Parâmetro ausente (add/play)', true);
+            return;
+        }
+        
+        arr[0] = arr[0].toLowerCase();
+        
+        if ( arr[0] == '/add')
+            adicionar(parseInt(arr[1]));
+        
+        if ( arr[0] == '/play')
+            tocar(parseInt(arr[1]));
+    };
+    
+    API.on(API.CHAT_COMMAND, comando);
+})();

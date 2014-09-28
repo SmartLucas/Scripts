32bdfd7aa49c790f4fd192b939560b9d508e03f9
LuizEdP <luiz.eduardo@mrpbrasil.com>
2014-09-28 12:38:22 -0300

    Update mention.js

--- mention.js
+++ mention.js
@@ -1,1 +1,11 @@
-javascript: (function(){ $('head').append($('<audio />', {id: 'ping', src: 'http://www.mrpbrasil.com/Caipira/ContactAuthRequest.wav'})); API.on(API.CHAT, function(_){ _.type === 'mention' && document.getElementById('ping').play(); }) })();
+javascript: (function(){
+  
+    for (var i = 1; i <= 8; i++){
+      $('head').append(
+          $('<audio/>', {
+              id: 'som' + i, src: 'http://www.mrpbrasil.com/Caipira/sons/som' + i + '.mp3'})
+      );
+    }
+
+    API.on(API.CHAT, function(_){ _.type === 'mention' && 
+                      document.getElementById('som' + (Math.floor(Math.random()*8)+1)).play(); }) })();

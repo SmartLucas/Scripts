e0ab18977851ce84c1307ec34b7362639d7c4c2a
LuizEdP <luiz.eduardo@mrpbrasil.com>
2014-09-28 16:21:22 -0300

    Create mention2.js

---
+++ mention2.js
@@ -0,0 +1,107 @@
+    var total = 12,
+        escolhido = 0;
+        
+    for (var i = 1; i <= total; i++){
+      $('head').append(
+          $('<audio/>', {
+              id: 'som' + i, src: 'http://www.mrpbrasil.com/Caipira/sons/som' + i + '.mp3'})
+      );
+    }
+
+    function mencao(obj){
+        if (obj.type == 'mention')
+            document.getElementById('som' + ( escolhido == 0 ? (Math.floor(Math.random()*total)+1) : escolhido)).play();
+    };
+    
+    function adicionar(val){
+        if ( isNaN(val))
+            return;
+        
+        if ( val <= 0 ){
+            API.chatLog('Nenhum som para adicionar!', true);
+            return;
+        }
+        
+        if ( val <= total ){
+             if ( document.getElementById('som' + val))
+                document.getElementById('som' + val).remove();
+            
+             $('head').append(
+                  $('<audio/>', {
+                      id: 'som' + val, src: 'http://www.mrpbrasil.com/Caipira/sons/som' + val + '.mp3'})
+              );
+        }
+        else{
+            for (var i = total+1; i <= val; i++){
+                 $('head').append(
+                      $('<audio/>', {
+                          id: 'som' + i, src: 'http://www.mrpbrasil.com/Caipira/sons/som' + i + '.mp3'})
+                  );
+            }
+        }
+        total = (val > total ? val : total);
+        API.chatLog('Som(s) adicionado(s).', false);
+    };
+    
+    function link(val){
+        if (!val)
+            return;
+
+        $('head').append(
+            $('<audio/>', {
+                id: 'som' + ++total, src: val})
+        );
+        
+        API.chatLog('Som(s) adicionado(s).', false);
+    };
+    
+    function setSom(val){
+        if ( isNaN(val) || val < 0 || val > total){
+            API.chatLog('Som inválido!', true);
+            return;
+        }
+        
+        escolhido = val;
+        API.chatLog('Som definido: ' + (val == 0 ? ' aleatório' : val), false);
+    }
+    
+    function tocar(val){
+        if ( !isNaN(val) && val >= 0 && val <= total)
+            document.getElementById('som' + ( val == 0 ? (Math.floor(Math.random()*total)+1) : val)).play();
+        else
+            API.chatLog('Valor inválido', true);
+    };
+    
+    function comando(obj){
+        var arr = obj.split(" ");
+        
+        if ( !arr )
+            return;
+        
+        if ( !arr[1]){
+            API.chatLog('Parâmetro ausente (add/play)', true);
+            return;
+        }
+        
+        arr[0] = arr[0].toLowerCase();
+        
+        if ( arr[0] == '/add')
+            adicionar(parseInt(arr[1]));
+        
+        if ( arr[0] == '/play')
+            tocar(parseInt(arr[1]));
+        
+        if ( arr[0] == '/set')
+            setSom(parseInt(arr[1]));
+        
+        if ( arr[0] == '/link')
+            link(arr[1]);
+            
+    };
+    
+    API.on(API.CHAT, mencao);
+    API.on(API.CHAT_COMMAND, comando);
+    
+    API.chatLog('Delícia de menção ativada, cara!', true);
+    API.chatLog('Comandos: /play [0-12] = toca o som correspondente ao número, 0 para aleatório;', true);
+    API.chatLog('/set [0-12] = define um som fixo para tocar quando for mencionado, 0 para aleatório', true);
